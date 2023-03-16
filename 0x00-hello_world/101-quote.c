3e38ce7426b4d9dcc2d7d20858dba89920959e3a
AymanAegon <ayo12ammar24@gmail.com>
2023-03-16 12:11:31 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,13 @@
  */
 int main(void)
 {
-	char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[31] = "and that piece of art is useful";
+	char str2[28] = "\" - Dora Korpar, 2015-10-19\n";
 	int i;
-	for(i=0; i<59; i++){
-		putchar(str[i]);
-	}
+
+	for( i = 0; i < 31; i++)
+		putchar(str1[i]);
+	for( i = 0; i < 28; i++)
+		putchar(str2[i]);
 	return (1);
 }
