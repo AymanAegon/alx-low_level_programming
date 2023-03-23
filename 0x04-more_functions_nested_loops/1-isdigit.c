8116d395a7c5719fba7b7ea1df8f10cf011b6011
AymanAegon <ayo12ammar24@gmail.com>
2023-03-23 13:02:47 -0700

    Write a function that checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _isupper -  checks for a digit
+* @c: the digit
+*
+* Return: 1 if a digit else 0
+*/
+int _isdigit(int c)
+{
+	return (isdigit(c));
+}
