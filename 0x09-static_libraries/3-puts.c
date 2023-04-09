b8ce9d61e01fe9c6018e352acdb079a37c6ca258
AymanAegon <ayo12ammar24@gmail.com>
2023-04-09 08:55:12 -0700

    Create the static library libmy.a containing all the functions

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,3 +1,12 @@
-void _puts(char *s)
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: the string
+ * Return: void
+ */
+
+void _puts(char *str)
 {
+	puts(str);
 }
