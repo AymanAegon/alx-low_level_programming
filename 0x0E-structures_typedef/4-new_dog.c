eea55a76646a0cbcad7ba90bed57e03729b40d60
AymanAegon <ayo12ammar24@gmail.com>
2023-04-17 08:43:48 -0700

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,6 +18,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 	d->age = age;
+	d->name = (char *)malloc(sizeof(char) * (strlen(name) + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = (char *)malloc(sizeof(char) * (strlen(owner) + 1));
+	if (d->owner == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
 	strcpy(d->name, name);
 	strcpy(d->owner, owner);
 	return (d);
