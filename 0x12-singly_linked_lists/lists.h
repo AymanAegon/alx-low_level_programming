4a7e8a45b0ef2e6c52a3ddd3924ba40f344273a8
AymanAegon <ayo12ammar24@gmail.com>
2023-04-27 07:53:38 -0700

    Write a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 /**
  * struct list_s - singly linked list
@@ -15,4 +17,6 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+int _putchar(char c);
+size_t print_list(const list_t *h);
 #endif
