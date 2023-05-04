cdf2eb1e80e4caad2e7dffa1365437f7a588c788
AymanAegon <ayo12ammar24@gmail.com>
2023-05-04 11:35:38 -0700

    Write a function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-#include <math.h>
+
+/**
+ * _pow - calculate the power
+ * @x: base
+ * @y: power
+ *
+ * Return: x^y
+ */
+unsigned int _pow(int x, unsigned int y)
+{
+	unsigned int i, s = 1;
+	for (i = 0; i < y; i++)
+		s *= x;
+	return (s);
+}
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: pointer to a string of 0 and 1 chars
@@ -21,8 +35,8 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] != '1' && b[i] != '0')
 			return (0);
 		n--;
-		if(b[i] == '1')
-			s += pow(2, n);
+		if (b[i] == '1')
+			s += _pow(2, n);
 	}
 	return (s);
 }
