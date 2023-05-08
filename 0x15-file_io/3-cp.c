8b1762f90b540ed93f0b0cc2a9ad2697cf76f979
AymanAegon <ayo12ammar24@gmail.com>
2023-05-08 10:22:00 -0700

    Write a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
  */
 int main(int ac, char **av)
 {
-    int f_to, f_from, n;
+    int f_to, f_from, r, w, n;
     char *buf;
 
     if (ac != 3)
@@ -18,33 +18,38 @@ int main(int ac, char **av)
     	dprintf(2, "Usage: cp file_from file_to\n");
         exit(97);
     }
-    f_from = open(av[1], O_RDONLY);
-    if (f_from == -1)
-    {
-    	dprintf(2, "Error: Can't read from file %s\n", av[1]);
-    	exit(98);
-	}
-    f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (f_to == -1)
-    {
-    	dprintf(2, "Error: Can't write to %s\n", av[2]);
-    	exit(99);
-	}
+
 	buf = malloc(sizeof(char) * 1024);
-	n = read(f_from, buf, 1024);
-	if (n == -1)
+	if (buf == NULL)
 	{
-    	dprintf(2, "Error: Can't read from file %s\n", av[1]);
-    	free(buf);
-		exit(98);
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
 	}
-	n = write(f_to, buf, n);
-    if (n == -1)
-    {
-    	dprintf(2, "Error: Can't write to %s\n", av[2]);
-    	free(buf);
-    	exit(99);
-	}
+    f_from = open(av[1], O_RDONLY);
+	r = read(f_from, buf, 1024);
+	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	do {
+		if (f_from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			free(buf);
+			exit(98);
+		}
+
+		w = write(to, buf, r);
+		if (f_to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			free(buf);
+			exit(99);
+		}
+
+		r = read(from, buf, 1024);
+		f_to = open(av[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+	free(buf);
 	n = close(f_from);
 	if (n == -1)
 	{
