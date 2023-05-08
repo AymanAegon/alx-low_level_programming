8fa8a98a0896f930d160a26c3e28af9eba6eab3e
AymanAegon <ayo12ammar24@gmail.com>
2023-05-08 10:41:01 -0700

    Write a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+ * close_file - Closes file descriptors.
+ * @f: The file descriptor to be closed.
+ */
+void close_file(int f)
+{
+	if (close(f) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f);
+		exit(100);
+	}
+}
 
 /**
  * main - copies the content of a file to another file.
@@ -10,25 +22,23 @@
  */
 int main(int ac, char **av)
 {
-    int f_to, f_from, r, w, n;
-    char *buf;
+	int f_to, f_from, r, w;
+	char *buf;
 
-    if (ac != 3)
-    {
-    	dprintf(2, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	buf = malloc(sizeof(char) * 1024);
 	if (buf == NULL)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-    f_from = open(av[1], O_RDONLY);
+	f_from = open(av[1], O_RDONLY);
 	r = read(f_from, buf, 1024);
 	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	do {
 		if (f_from == -1 || r == -1)
 		{
@@ -36,7 +46,6 @@ int main(int ac, char **av)
 			free(buf);
 			exit(98);
 		}
-
 		w = write(f_to, buf, r);
 		if (f_to == -1 || w == -1)
 		{
@@ -44,23 +53,11 @@ int main(int ac, char **av)
 			free(buf);
 			exit(99);
 		}
-
 		r = read(f_from, buf, 1024);
 		f_to = open(av[2], O_WRONLY | O_APPEND);
-
 	} while (r > 0);
 	free(buf);
-	n = close(f_from);
-	if (n == -1)
-	{
-    	dprintf(2, "Error: Can't close fd %d\n", f_from);
-    	exit(100);
-	}
-	n = close(f_to);
-	if (n == -1)
-	{
-    	dprintf(2, "Error: Can't close fd %d\n", f_to);
-    	exit(100);
-	}
-    return (0);
+	close_file(f_from);
+	close_file(f_to);
+	return (0);
 }
