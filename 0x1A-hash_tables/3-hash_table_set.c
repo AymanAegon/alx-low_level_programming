51f73ad5c07e3803756d93dbb3424286a2a79e4a
AymanAegon <ayo12ammar24@gmail.com>
2023-07-07 10:19:37 -0700

    pushing files

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: the hash table
+ * @key: the key. key can not be an empty string
+ * @value: the value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index, i;
+	hash_node_t *node;
+
+	if (*key == '\0')
+		return (0);
+	index = key_index(key, ht->size);
+
+	node = malloc(sizeof(*node));
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
+}
