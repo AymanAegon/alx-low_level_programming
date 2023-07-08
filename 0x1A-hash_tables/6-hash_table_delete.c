4fdd953f331fc282a05889693fe2d0e4a8d90d65
AymanAegon <ayo12ammar24@gmail.com>
2023-07-08 10:39:14 -0700

    Write a function that deletes a hash table.

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * delete_list - deletes a linked list
+ * @head: the head of a linkedlist
+ *
+ * Return: void
+ */
+void delete_list(hash_node_t *head)
+{
+	if (!head)
+		return;
+	delete_list(head->next);
+	free(head->key);
+	free(head->value);
+	free(head);
+}
+
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: the hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+		delete_list(ht->array[i]);
+}
