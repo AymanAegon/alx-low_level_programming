2b7b01c9a46c32e556c5af2ae6e2896b7966592b
AymanAegon <ayo12ammar24@gmail.com>
2023-10-10 04:29:05 -0700

    9

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -23,7 +23,7 @@ int interpolation_search(int *array, size_t size, int value)
 	{
 		pos = low + (((double)(high - low)
 		/ (array[high] - array[low])) * (value - array[low]));
-		printf("Value checked array[%d]", pos);
+		printf("Value checked array[%ld]", pos);
 		if (pos >= size)
 		{
 			printf(" is out of range\n");
